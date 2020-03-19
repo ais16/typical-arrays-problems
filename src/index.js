d5a5d708c51ede230d9931decaaf7e90ba6f7fdb
ais16 <54989705+ais16@users.noreply.github.com>
2020-03-19 16:42:38 +0600

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) return 0;
+    return array.reduce((prev, current) => Math.min(prev, current));
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) return 0;
+    return array.reduce((prev, current) => Math.max(prev, current));
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) return 0;
+    return array.reduce((sum, i) => sum + i) / array.length;
+};
